9f73c7dc042a3ba68744cacd507292fe746acb10
Virginia Aquino <37305214+virginiayjd7@users.noreply.github.com>
2020-10-26 17:40:29 -0500

    final

---
+++ ApplicationBiometría_Aquino/src/applicationbiometría_aquino/ApplicationBiometría_Aquino.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package applicationbiometría_aquino;
+
+/**
+ *
+ * @author Virginia Aquino
+ */
+public class ApplicationBiometría_Aquino {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       Biometría ventana = new Biometría();
+         ventana.setVisible(true);
+    }
+    
+}
